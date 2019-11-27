230468c42bf57e09d1bf24a72bd23c782746ac22
Peiran Quan <quanpeiran@gmail.com>
2019-11-27 17:03:21 +1300

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,9 +1,10 @@
 'use strict';
-
+/*
 // [START gae_node_request_example]
 const express = require('express');
 
 const app = express();
+app.use('/static', express.static('BuildFromScratch'));
 
 app.get('/', (req, res) => {
   res
@@ -11,6 +12,28 @@ app.get('/', (req, res) => {
     .send('Hello, worlddd!')
     .end();
 });
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END gae_node_request_example]
+
+module.exports = app;
+*/
+
+// [START gae_node_request_example]
+const express = require('express');
+
+const app = express();
+app.use('/static', express.static('BuildFromScratch'));
+
+app.get('/', (req, res) => {
+  res.status(200)
+    .sendFile(__dirname + '/index.html'); //finally -- worked by getting rid of end() & status
+});                              
 
 // Start the server
 const PORT = process.env.PORT || 8080;
